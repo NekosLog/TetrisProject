b930c5d88436275dff83e9b6edb3b6e91c595989
STD\s20227002 <shironeko248@gmail.com>
2023-10-26 10:41:22 +0900

    InputManager追加・作成開始

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static int _inputValue_Right;
+
+    private static int _inputValue_Left;
+
+    private static int _inputValue_Down;
+
+    private static int _inputValue_Up;
+
+    private static int _inputValue_Dicision;
+
+    private static int _inputValue_Cancel;
+}
