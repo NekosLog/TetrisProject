f327682272b837f41510e956469ce1d59376e374
STD\s20227002 <20227002-nishigakitakuya@hcs.ac.jp>
2023-11-13 15:02:28 +0900

    いったん完成

--- Assets/Scripts/InterFace/IFGetMinoArray.cs
+++ Assets/Scripts/InterFace/IFGetMinoArray.cs
@@ -1,4 +1,11 @@
+/// <summary>
+/// ArrayControllerから配列を取得するためのインターフェース
+/// </summary>
 interface IFGetMinoArray
 {
+    /// <summary>
+    /// 停止中のミノの配列を取得するメソッド
+    /// </summary>
+    /// <returns>停止中のミノの配列</returns>
     MinoData.E_MinoColor[,] GetMinoArray();
 }
